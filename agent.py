b60a885cdee79f385378c8b0e6e1e2be7da8b2c8
Olga Giannouchou <115019595+olgagnn@users.noreply.github.com>
2025-11-27 21:41:40 +0000

    ceo

--- agent.py
+++ agent.py
@@ -1,9 +1,14 @@
 from llm import llm
 from graph import graph
-
-# Create a movie chat chain
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
+from langchain.tools import Tool
+from langchain_neo4j import Neo4jChatMessageHistory
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain import hub
+from utils import get_session_id
 
 chat_prompt = ChatPromptTemplate.from_messages(
     [
@@ -14,9 +19,6 @@ chat_prompt = ChatPromptTemplate.from_messages(
 
 movie_chat = chat_prompt | llm | StrOutputParser()
 
-# Create a set of tools
-from langchain.tools import Tool
-
 tools = [
     Tool.from_function(
         name="General Chat",
@@ -24,19 +26,48 @@ tools = [
         func=movie_chat.invoke,
     )
 ]
-
-# Create chat history callback
-from langchain_neo4j import Neo4jChatMessageHistory
-
 def get_memory(session_id):
     return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
 
-# Create the agent
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain import hub
+agent_prompt = PromptTemplate.from_template("""
+You are a movie expert providing information about movies.
+Be as helpful as possible and return as much information as possible.
+Do not answer any questions that do not relate to movies, actors or directors.
 
-agent_prompt = hub.pull("hwchase17/react-chat")
+Do not answer any questions using your pre-trained knowledge, only use the information provided in the context.
+
+TOOLS:
+------
+
+You have access to the following tools:
+
+{tools}
+
+To use a tool, please use the following format:
+
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+```
+
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+Final Answer: [your response here]
+```
+
+Begin!
+
+Previous conversation history:
+{chat_history}
+
+New input: {input}
+{agent_scratchpad}
+""")
+
 agent = create_react_agent(llm, tools, agent_prompt)
 agent_executor = AgentExecutor(
     agent=agent,
@@ -50,9 +81,6 @@ chat_agent = RunnableWithMessageHistory(
     input_messages_key="input",
     history_messages_key="chat_history",
 )
-
-# Create a handler to call the agent
-from utils import get_session_id
 
 def generate_response(user_input):
     """
